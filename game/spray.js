df0abc4b9b4d2fc74964069637bde2ba74a1eb1b
dkaushik <dkaushik@watermarkinsights.com>
2021-01-22 21:05:01 +0530

    added spray sound

--- game/spray.js
+++ game/spray.js
@@ -9,22 +9,22 @@ spray.bulletDistance = 1000;
 //spray.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
 spray.bulletKillType = Phaser.Weapon.KILL_DISTANCE;
 //  The speed at which the bullet is fired
-spray.bulletSpeed = 1500;
+spray.bulletSpeed = 1000;
 
 //spray.bulletRotateToVelocity = true;
 
 //spray.fireAngle = -180;
 
 //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 50ms
-spray.fireRate = 90;
+spray.fireRate = 20;
 
 spray.onFire.add(function(){
-  //blaster.play();
+  spraySound.play();
 });
 
 //  Tell the Weapon to track the 'player' Sprite
 //  With no offsets from the position
 //  But the 'true' argument tells the weapon to track sprite rotation
-spray.trackSprite(player, 130, 140, true);
+spray.trackSprite(player, 150, 140, true);
 
 }
