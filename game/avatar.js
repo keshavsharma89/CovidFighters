078852691c660eb284db14e1213667b856265cfd
amsingh <amsingh@watermarkinsights.com>
2021-01-22 17:31:33 +0530

    Integrate the character selection page with game

--- game/avatar.js
+++ game/avatar.js
@@ -1,106 +1,58 @@
-var game;
-var speedMult = 0.7;
-var friction = 0.99;
-var startGame;
-var counter  =0;
 var selectedCharacter;
 var start_game;
-//var colors = ["0xac81bd","0xff5050"];
-
-
- var game = new Phaser.Game(800, 700, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 
 
-
+var game = new Phaser.Game(1320, 600, Phaser.AUTO, 'phaser-example',
+ { preload: preload, create: create, update: update });
 
 function preload(){
-  console.log('----------- preload ------------');
-  console.log(game);
-       game.load.spritesheet('biden', '../assets/biden_gif.gif', { frameWidth: 38, frameHeight: 56 });
-       game.load.spritesheet("modi", "../assets/modi_gif.gif",{ frameWidth: 38, frameHeight: 56 });
-     game.load.image("avatar_bg", "../assets/avatar.jpeg");
-     game.load.spritesheet("avatar_select", "../assets/avatar_select.png");
-     game.load.spritesheet("start_game", "../assets/start_game.png");
+    game.load.atlasJSONHash('biden', '../assets/biden_spritesheet.png', '../assets/biden_spritesheet.json');
+    game.load.atlasJSONHash('modi', '../assets/modi_spritesheet.png', '../assets/modi_spritesheet.json');
+    game.load.image("avatar_bg", "../assets/avatar.jpeg");
+    game.load.spritesheet("avatar_select", "../assets/avatar_select.png");
+    game.load.spritesheet("start_game", "../assets/start_game.png");
 }
 
 function create(){
+  game.add.tileSprite(0, 0, 1320, 600 , 'avatar_bg');
+  let select_avatar = game.add.sprite((game.width /2)-200, 50, 'avatar_select');
+  let biden_image = game.add.sprite((game.width /2)-150, 200, 'biden');
+  let modi_image = game.add.sprite((game.width /2)+150, 200, 'modi');
+  biden_image.animations.add('dance_biden');
+  modi_image.animations.add('dance_modi');
+  biden_image.inputEnabled = true;
+  modi_image.inputEnabled = true;
 
-  game.add.tileSprite(0, 0, 800, 700 , 'avatar_bg');
-    //game.stage.backgroundColor = "#000044";
-
-    console.log(this);
-    var select_avatar = game.add.sprite(150, 50, 'avatar_select');
-    var biden_image = game.add.sprite(150, 200, 'biden');
-    var modi_image = game.add.sprite(400, 200, 'modi');
-  // biden_image.rotation = 0.14;
-   //modi_image.rotation = 0.14;
-   biden_image.inputEnabled = true;
-   modi_image.inputEnabled = true;
-
-//   text = game.add.text(game.width /2,game.height -100,'', {font: "18px Arial", fill: "#ffffff"})
-   biden_image.events.onInputDown.add(selectCharacter);
-   modi_image.events.onInputDown.add(selectCharacter);
+  biden_image.animations.play('dance_biden', 10, true);
+  modi_image.animations.play('dance_modi', 10, true);
+  biden_image.events.onInputDown.add(selectCharacter);
+  modi_image.events.onInputDown.add(selectCharacter);
 
 }
 
 function selectCharacter(item, pointer)
 {
-    console.log(item.key);
     this.selectedCharacter = item.key;
     if(start_game)
     {
       start.kill();
     }
-     start_game = game.add.sprite(250, game.height-200, 'start_game');
-  //  this.add.sprite(150, 150, 'start_game');
-  //  text.text = "Start game with Covid Fight " + this.selectedCharacter ;
+    start_game = game.add.sprite((game.width /2)+40, game.height-100, 'start_game');
+    start_game.anchor.set(0.5);
+
 }
 
 
 function update(){
-    // var zoomed = false;
-    // for(var i = 0; i < this.scrollingMap.children.length; i++){
-    //      if(Math.abs(this.scrollingMap.children[i].world.x - game.width / 2) < 46 && !zoomed){
-    //           this.scrollingMap.getChildAt(i).scale.setTo(1.3);
-    //           zoomed = true;
-    //      }
-    //      else{
-    //           this.scrollingMap.getChildAt(i).scale.setTo(1);
-    //      }
-    // }
-    // if(this.scrollingMap.isBeingDragged){
-    //      this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
-    // }
-    // else{
-    //      if(this.scrollingMap.movingSpeed > 1){
-    //           this.scrollingMap.x += this.scrollingMap.movingSpeed * Math.cos(this.scrollingMap.movingangle);
-    //           if(this.scrollingMap.x < game.width - this.scrollingMap.width){
-    //                this.scrollingMap.x = game.width - this.scrollingMap.width;
-    //                this.scrollingMap.movingSpeed *= 0.5;
-    //                this.scrollingMap.movingangle += Math.PI;
 
-    //           }
-    //           if(this.scrollingMap.x > 0){
-    //                this.scrollingMap.x = 0;
-    //                this.scrollingMap.movingSpeed *= 0.5;
-    //                this.scrollingMap.movingangle += Math.PI;
-    //           }
-    //           this.scrollingMap.movingSpeed *= friction;
-    //           this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
-    //      }
-    //      else{
-    //           var distance = this.scrollingMap.savedPosition.distance(this.scrollingMap.position);
-    //           var angle = this.scrollingMap.savedPosition.angle(this.scrollingMap.position);
-    //           if(distance > 4){
-    //                this.scrollingMap.movingSpeed = distance * speedMult;
-    //                this.scrollingMap.movingangle = angle;
-    //           }
-    //      }
-    // }
-}
-
-function render()
-{
+  if (this.start_game && game.input.activePointer.isDown)
+    {
+        this.start_game.alpha = 0.5;
+    }
+    else if(this.start_game)
+    {
+        this.start_game.alpha = 1;
+    }
 
 }
--- game/game.js
+++ game/game.js
@@ -2,10 +2,19 @@
 var game = new Phaser.Game(1320, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
+    //preload for player selection starts
+    game.load.atlasJSONHash('biden', '../assets/biden_spritesheet.png', '../assets/biden_spritesheet.json');
+    game.load.atlasJSONHash('modi', '../assets/modi_spritesheet.png', '../assets/modi_spritesheet.json');
+    game.load.image("avatar_bg", "../assets/avatar.jpeg");
+    game.load.spritesheet("avatar_select", "../assets/avatar_select.png");
+    game.load.spritesheet("start_game", "../assets/start_game.png");
+    //preload for player selection starts
+
     game.load.image('backgroung', '../assets/Game_background.png');
     game.load.image('covid', '../assets/covid.png');
     game.load.image('sanitizer','../assets/sanitizer.png');
-    game.load.spritesheet('player', '../assets/modi1152.png', 127, 190);
+    game.load.spritesheet('modi_avatar', '../assets/modi1152.png', 127, 190);
+    game.load.spritesheet('biden_avatar', '../assets/biden1152.png', 127, 190);
     //game.load.spritesheet('player', '../assets/bigger-modi.png', 38, 56);
     //game.load.tilemap('map', '../assets/tiles.json', null, Phaser.Tilemap.TILED_JSON);
     //game.load.image('ground_1x1', '../assets/ground_1x1.png');
@@ -15,6 +24,7 @@ function preload() {
 var map;
 var layer;
 var cursors;
+var player;
 var sprayButton;
 var jumpTimer = 0;
 var facing='';
@@ -22,8 +32,41 @@ var sanitizer;
 var covid;
 var spray;
 var vaccine;
+var selectedCharacter;
+var start_game;
 
-function create() {
+
+function create(){
+  game.add.tileSprite(0, 0, 1320, 600 , 'avatar_bg');
+  let select_avatar = game.add.sprite((game.width /2)-200, 50, 'avatar_select');
+  let biden_image = game.add.sprite((game.width /2)-150, 200, 'biden');
+  let modi_image = game.add.sprite((game.width /2)+150, 200, 'modi');
+  biden_image.animations.add('dance_biden');
+  modi_image.animations.add('dance_modi');
+  biden_image.inputEnabled = true;
+  modi_image.inputEnabled = true;
+
+  biden_image.animations.play('dance_biden', 10, true);
+  modi_image.animations.play('dance_modi', 10, true);
+  biden_image.events.onInputDown.add(selectCharacter);
+  modi_image.events.onInputDown.add(selectCharacter);
+
+}
+
+
+function selectCharacter(item, pointer)
+{
+    this.selectedCharacter = item.key;
+
+    start_game = game.add.sprite((game.width /2)+40, game.height-100, 'start_game');
+    start_game.anchor.set(0.5);
+    start_game.inputEnabled = true;
+    start_game.events.onInputDown.add(startGame);
+
+}
+
+function startGame(item, pointer) {
+  console.log('-- start game ---');
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.tileSprite(0, 0, 7000, 500 , 'backgroung');
@@ -41,8 +84,14 @@ function create() {
     //layer.resizeWorld();
 
     //map.setCollisionBetween(1, 34);
+    if(this.selectedCharacter == 'biden')
+    {
+      player = game.add.sprite(100, 350, 'biden_avatar');  
+    }else
+    {
+      player = game.add.sprite(100, 350, 'modi_avatar');
+    }
 
-    player = game.add.sprite(100, 350, 'player');
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
     player.body.bounce.y = 0.2;
@@ -91,60 +140,63 @@ function create() {
 
 
 function update() {
+  if(player)
+  {
 
-  player.body.velocity.x = 0;
-  game.physics.arcade.collide(player, layer);
-    if (cursors.left.isDown)
-    {
-        player.body.velocity.x = -150;
+    player.body.velocity.x = 0;
+    game.physics.arcade.collide(player, layer);
+      if (cursors.left.isDown)
+      {
+          player.body.velocity.x = -150;
 
-        if (facing != 'left')
-        {
-            player.animations.play('left');
-            facing = 'left';
-        }
-    }
-    else if (cursors.right.isDown)
-    {
-        player.body.velocity.x = 150;
-
-        if (facing != 'right')
-        {
-            player.animations.play('right');
-            facing = 'right';
-        }
-    }
-    else
-    {
-        if (facing != 'idle')
-        {
-            player.animations.stop();
-
-            if (facing == 'left')
-            {
-                player.frame = 4;
-            }
-            else
-            {
-                player.frame = 4;
-            }
-
-            facing = 'idle';
-        }
-    }
-    if (sprayButton.isDown){
-              spray.fire();
+          if (facing != 'left')
+          {
+              player.animations.play('left');
+              facing = 'left';
           }
+      }
+      else if (cursors.right.isDown)
+      {
+          player.body.velocity.x = 150;
 
-    if (cursors.up.isDown && player.body.onFloor() && game.time.now > jumpTimer)
-    {
-        player.body.velocity.y = -250;
-        jumpTimer = game.time.now + 750;
-    }
-    game.physics.arcade.overlap(sanitizer, player, sanitizerCollisionHandler, null, this);
-    game.physics.arcade.overlap(vaccine, player, vaccineCollisionHandler, null, this);
-    game.physics.arcade.collide(spray.bullets, covid, collisionHandler);
+          if (facing != 'right')
+          {
+              player.animations.play('right');
+              facing = 'right';
+          }
+      }
+      else
+      {
+          if (facing != 'idle')
+          {
+              player.animations.stop();
 
+              if (facing == 'left')
+              {
+                  player.frame = 4;
+              }
+              else
+              {
+                  player.frame = 4;
+              }
+
+              facing = 'idle';
+          }
+      }
+      if (sprayButton.isDown){
+                spray.fire();
+            }
+
+      if (cursors.up.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+      {
+          player.body.velocity.y = -250;
+          jumpTimer = game.time.now + 750;
+      }
+      game.physics.arcade.overlap(sanitizer, player, sanitizerCollisionHandler, null, this);
+      game.physics.arcade.overlap(vaccine, player, vaccineCollisionHandler, null, this);
+      game.physics.arcade.collide(spray.bullets, covid, collisionHandler);
+
+  }
 
 }
 function vaccineCollisionHandler(player, vaccine){
